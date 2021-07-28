bf3f6cfaa59ed9d8e22a646a448fc2efab4434de
Emily Westerback <emilywesterback2@gmail.com>
2021-07-28 09:49:39 -0700

    learn more formatted

--- portfolio/src/pages/Connect.js
+++ portfolio/src/pages/Connect.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import github from '../photos/githubLogo.png'
-import linkedin from '../photos/linkedinlogo.png'
+import linkedin from '../photos/linkedinlogo.jpeg'
 
 const Connect = (props) => {
   const onResumeClick = () => {
@@ -8,16 +8,31 @@ const Connect = (props) => {
   }
   return (
     <div className="connect">
-      <h2>Learn More About Me</h2>
-      <button onClick={onResumeClick}>View Resume</button>
-      <img src={linkedin} alt="Linkedin" />
-      <a href="https://www.linkedin.com/in/emily-westerback/" target="_blank">
-        /emily-westerback
-      </a>
-      <img className="logo" src={github} alt="GitHub" />
-      <a href="https://github.com/ewesterback" target="_blank">
-        /ewesterback
-      </a>
+      <div className="top-rectangle"></div>
+      <div className="bottom-rectangle"></div>
+      <div className="top-right-rectangle"></div>
+      <div className="bottom-left-rectangle"></div>
+      <div className="connect-content">
+        <h2>Learn More About Me</h2>
+        <button onClick={onResumeClick}>View Resume</button>
+        <div className="connect-links">
+          <div className="logo-group">
+            <img className="logo" src={linkedin} alt="Linkedin" />
+            <a
+              href="https://www.linkedin.com/in/emily-westerback/"
+              target="_blank"
+            >
+              /emily-westerback
+            </a>
+          </div>
+          <div className="logo-group">
+            <img className="logo" src={github} alt="GitHub" />
+            <a href="https://github.com/ewesterback" target="_blank">
+              /ewesterback
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- portfolio/src/pages/Resume.js
+++ portfolio/src/pages/Resume.js
@@ -7,8 +7,10 @@ const Resume = (props) => {
   }
   return (
     <div className="resume">
-      <button onClick={onBackClick}>Back to main page</button>
-      <h2>Resume</h2>
+      <div className="resume-top">
+        <button onClick={onBackClick}>Back to main page</button>
+        <h2>Emily Westerback Resume</h2>
+      </div>
       <iframe src={resume} width="100%" height="600px"></iframe>
     </div>
   )
